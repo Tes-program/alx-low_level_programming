19aa4547c3527c95cb69471014e5c3377b20eace
Tes-program <teslimodumuyiwa@gmail.com>
2022-04-08 21:30:38 +0100

    omooo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 #include <string.h>
 
 int main() {
-    // Write a program that print "Programming is like building a multilingual puzzle
     char str[] = "\"Programming is like building a multilingual puzzle";
     puts(str);
     return 0;
